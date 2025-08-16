373307a23b6bc3964c8072923d38090146ec18d6
Jmeow <j@jmeow.net>
2025-08-16 09:32:29 -0500

    Add background

--- public/flappyBird.js
+++ public/flappyBird.js
@@ -7,12 +7,18 @@ function imageAsset(name) {
 }
 const assets = {
     base: imageAsset("base"),
+    dayBG: imageAsset("background-day"),
 }
 let groundTick = 0
 let lastUpdate = Date.now()
 function draw() {
     requestAnimationFrame(draw)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i <= Math.ceil(canvas.width / 288); i++) {
+        ctx.drawImage(assets.dayBG, i * 288, canvas.height - 512)
+    }
+    ctx.fillStyle = "#4dc1cb"
+    ctx.fillRect(0, 0, canvas.width, canvas.height - 512)
     for (let i = 0; i <= Math.ceil(canvas.width / 336); i++) {
         ctx.drawImage(
             assets.base,
