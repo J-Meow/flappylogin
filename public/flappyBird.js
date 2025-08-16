33bed50c946df3e3f6a9597b1192b6f5d8d24c4d
Jmeow <j@jmeow.net>
2025-08-16 09:17:33 -0500

    Set up canvas and draw/update loop to draw ground

---
+++ public/flappyBird.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
+function imageAsset(name) {
+    const elem = document.createElement("img")
+    elem.src = "/assets/sprites/" + name + ".png"
+    return elem
+}
+const assets = {
+    base: imageAsset("base"),
+}
+let groundTick = 0
+let lastUpdate = Date.now()
+function draw() {
+    requestAnimationFrame(draw)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i <= Math.ceil(canvas.width / 336); i++) {
+        ctx.drawImage(
+            assets.base,
+            -Math.floor(groundTick) + i * 336,
+            canvas.height - 112,
+        )
+    }
+}
+function update() {
+    const millisecondsSinceUpdate = Date.now() - lastUpdate
+    const movementX = millisecondsSinceUpdate / 4
+    groundTick = (groundTick + movementX) % 24
+    lastUpdate = Date.now()
+    setTimeout(update, 1000 / 60)
+}
+draw()
+update()
