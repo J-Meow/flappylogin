f62a451ddc1b4c2ae2994514d10e9cac5222af06
Jmeow <j@jmeow.net>
2025-08-29 22:46:04 -0500

    Add alerts for account creation

--- public/signup/signup.js
+++ public/signup/signup.js
@@ -1,18 +1,26 @@
-document.getElementById("signup-form").addEventListener("submit", (ev) => {
-    ev.preventDefault()
-    if (
-        document.getElementById("password").value !=
-        document.getElementById("confirm-password").value
-    ) {
-        alert("Passwords do not match!")
-        return
-    }
-    fetch("/api/signup", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value,
-        }),
+document
+    .getElementById("signup-form")
+    .addEventListener("submit", async (ev) => {
+        ev.preventDefault()
+        if (
+            document.getElementById("password").value !=
+            document.getElementById("confirm-password").value
+        ) {
+            alert("Passwords do not match!")
+            return
+        }
+        const res = await fetch("/api/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: document.getElementById("username").value,
+                password: document.getElementById("password").value,
+            }),
+        })
+        const json = await res.json()
+        if (!json.ok) {
+            alert("Error: " + json.error)
+        } else {
+            alert("Account created")
+        }
     })
-})
