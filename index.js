29bcaec37e7da6466c4e59be527f79c9b38b4618
Jmeow <j@jmeow.net>
2025-08-29 21:40:06 -0500

    Receive JSON body

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express()
 const port = 3000
 
 app.use("/", express.static("public"))
+app.use(express.json())
 
 //app.get("/", (req, res) => {
 //    res.send("Hello World!")
 //})
+
+app.post("/api/signup", (req, res) => {
+    console.log(req.body)
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
