7baf0d4efc118b50b003b2cad8fdbf97badf29df
Jmeow <j@jmeow.net>
2025-08-29 21:29:47 -0500

    Send request to nonexistent signup api

--- public/signup/signup.js
+++ public/signup/signup.js
@@ -1,3 +1,17 @@
 document.getElementById("signup-form").addEventListener("submit", (ev) => {
     ev.preventDefault()
+    if (
+        document.getElementById("password").value !=
+        document.getElementById("confirm-password").value
+    ) {
+        alert("Passwords do not match!")
+        return
+    }
+    fetch("/api/signup", {
+        method: "POST",
+        body: JSON.stringify({
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value,
+        }),
+    })
 })
